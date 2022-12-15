5d6f5d2f8fecdbf9046f89dfeabec0dbea7896f8
Anotn <antonrojkov_04@mail.ru>
2022-12-15 10:30:00 +0400

    teagrounds
    ДЕЛАЮ ПРОФИЛЬ

--- teagrounds/resources/views/home.blade.php
+++ teagrounds/resources/views/home.blade.php
@@ -1,23 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<head>
+<link rel="stylesheet" href="{{ asset("css/home.css") }}">
+</head>
+<div class="d-f">
+    <div class="navigation">
+        <div class="navigation-f1">
+            <li><a href="/" style="font-size:2.5vw;">Tea Grounds</a></li>
+        </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="navigation-f2">
+            <li><a href="/about">О нас</a></li>
+            <li><a href="/shop">Магазин</a></li>
+            <li><a href="/blog">Блог</a></li>
+            <li><a href="">Конструктор</a></li>
+        </div>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+        <div class="navigation-f3">
+            <li><a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }}</a>
+            </li>
+            <li><a href="">Заказы</a></li>
+            <li><a href="">История заказов</a></li>
+        </div>
+            
+        <div class="navigation-f4">
+            <li><a href="">Помощь</a></li>
+            <li>
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    {{ __('Выход') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
         </div>
     </div>
+    <div class="container">
+        <div class="point">
+            <p>Профиль</p>
+        </div>
+        <div class="info">
+            <div class="info-avatar">
+                <div class="elips"></div>
+                <p>Лиджиев Александр Олегович</p>
+            </div>
+        </div>
+        
+    </div>
 </div>
-@endsection
